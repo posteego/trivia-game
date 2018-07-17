bdfacaa3090b1e77510fa91ca9bd89a0cb03a562
kpostigo <7651554+kpostigo@users.noreply.github.com>
2018-07-16 20:09:08 -0400

    added categories

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,9 @@
-/* Trivia Game Logic */
+/* Jeopardy Game Logic */
 // Kenneth Postigo, July 2018
 
-// uses an API to get trivia questions
+// uses the jservice API to get trivia questions
+// using http://jservice.io/
 
-
-$(function() {
-// hide .game div on page load
-  $(".game").hide();
-});
-
-$(".start-btn").on('click', function() {
-// show game when play button gets clicked
-  $(".start").remove();   // remove play button
-  $(".game").show();      // show .game div
-});
 
 ////////////////////////////////////////////
 /////////////// Variables //////////////////
@@ -21,36 +11,9 @@ $(".start-btn").on('click', function() {
 
 // giphy api url + key
 // docs : https://developers.giphy.com/docs/
-var gifURL = '';
-var gifKEY = 'gmUoQcltu2OqZuLZ9RXHRKoT7hR8CHrk';
-
-// question/choice/answer objects
-var questions = [
-    {
-      question : '?',
-      choices: [''],
-      answer: 0
-    },
-
-    {
-      question : '?',
-      choices: [''],
-      answer: 0
-    },
-
-    {
-      question : '?',
-      choices: [''],
-      answer: 0
-    },
-
-    {
-      question : '?',
-      choices: [''],
-      answer: 0
-    }
-];
-
+var gifURL = '',
+    gifKEY = 'gmUoQcltu2OqZuLZ9RXHRKoT7hR8CHrk';
+var start = $(".start");
 
 ////////////////////////////////////////////
 //////////////// Utility ///////////////////
@@ -60,3 +23,28 @@ function randomize(min, max){
   return Math.floor(Math.random() * ( max - min + 1) + min);
 }
 
+
+////////////////////////////////////////////
+//////////////// Functions /////////////////
+////////////////////////////////////////////
+
+$(".start-btn").on('click', function() {
+// remove start button and show category options
+
+  $(".start-btn").remove();   // remove play button
+
+  // get categories from api
+  var catergories_url = 'http://jservice.io/api/categories?count=3';
+  $.ajax({
+    url : catergories_url,
+    method : 'GET'
+  }).then( function(categories) {
+    let btn = $('<button>');
+    btn.attr('type','button');
+
+    for (i in categories){
+      btn.text(categories[i].title);
+      btn.prependTo(start);
+    }
+  });
+});
